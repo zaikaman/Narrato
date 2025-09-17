7781df43f2c860ba899e6cbc008a954f14e8125a
zaikaman <91252891+zaikaman@users.noreply.github.com>
2025-09-17 23:35:23 +0700

    Aggressively stop zooming

--- static/main.js
+++ static/main.js
@@ -1,5 +1,19 @@
-        document.addEventListener('touchmove', function(event) {
-    if (event.scale !== 1) { event.preventDefault(); }
+        document.addEventListener('touchstart', function(event) {
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
+}, { passive: false });
+
+document.addEventListener('touchmove', function(event) {
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
+}, { passive: false });
+
+document.addEventListener('touchend', function(event) {
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
 }, { passive: false });
 
         let currentAudio = null;
